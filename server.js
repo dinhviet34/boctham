77514dbb21be3383d20e486df27012122ec58002
dinhviet34 <54301051+dinhviet34@users.noreply.github.com>
2024-02-19 17:18:25 +0700

    update

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 var express = require("express");
 var app = express();
 app.set('port', (process.env.PORT || 5000));
+app.use(express.json())
 app.use(express.static(__dirname + '/public'));
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	next();
 });
+
 app.set("view engine", "ejs");
 app.set("views", "./views");
 var server = require("http").Server(app);
@@ -22,25 +27,84 @@ io.on("connection", function (socket) {
 	socket.on("disconnect", function () {
 		console.log("disconnect");
 	});
+	socket.on("Joinroom",function(data){
+		socket.join(data);
+		io.sockets.in(data).emit("RoomAlert","ConnectedRoom " + data);
+		console.log("Welcome room " + data );
+	})
+	
 	//server lắng nghe dữ liệu từ client
 	socket.on("Client-sent-data", function (data) {
 		console.log(data);
+		var message  = data.split(" ")[0];
+		var room = data.split(" ")[1];
+		socket.join(room);
+		console.log(room);
 		//sau khi lắng nghe dữ liệu, server phát lại dữ liệu này đến các client khác
-		io.sockets.emit("Server-sent-data", data);
+		io.sockets.in(room).emit("Server-sent-data", message);
 	});
 	socket.on("Home-sent-data", function (data) {
 		console.log(data);
+		var message  = data.split(" ")[0];
+		var room = data.split(" ")[1];
+		socket.join(room);
 		//sau khi lắng nghe dữ liệu, server phát lại dữ liệu này đến các client khác
-		io.sockets.emit("Server-sent-home-ready", data);
+		io.sockets.in(room).emit("Server-sent-home-ready", message);
 	});
 });
+
+
 
 // create route, display view
 
 app.get("/", function (req, res) {
 	res.render("home");
 });
+app.post("/builder",function(req,res){
+	const {room,game,coupon} = req.body;
+	const data = req.body;
+	const accessToken = jwt.sign(data,process.env.ACCESS_TOKEN_SECRET,{
+		expiresIn:'2h',
+	});
+	
+	res.json({room:room,game:game,coupon:coupon,accessToken});
+	
 
-app.get("/button", function (rep, res) {
-	res.render("button");
+
+});
+app.get("/table/:room/:game/:coupon/:key", function (req, res) {
+	const roomnum = req.params.room;
+	const gamechar = req.params.game;
+	const coupon = req.params.coupon;
+	const authorizationClient = req.params.key;
+	const token = authorizationClient;
+  
+	if (!token) return res.sendStatus(401)
+  
+	try {
+		jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+		res.render("table",{room:roomnum,game:gamechar,coupon:coupon});
+	} catch (e) {
+		return res.sendStatus(403);
+	}
+	
+});
+
+app.get("/button/:room/:key", function (req, res) {
+	const roomnum = req.params.room;
+	const gamechar = req.params.game;
+	const authorizationClient = req.params.key;
+	const token = authorizationClient;
+  
+	if (!token) return res.sendStatus(401)
+  
+	try {
+		jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+		res.render("button",{room:roomnum});
+	} catch (e) {
+		return res.sendStatus(403);
+	}
+	
 })
+
+
